8e9ef9a8ea30e6088ed2eb3383376d21a101c449
JDjedi <JDjedi1201@gmail.com>
2016-08-18 22:47:13 -0500

    Added JS folder with workign jquery functions

---
+++ js/effects.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$(".title-box").hide();
+	$(".skills-box-1").hide();
+	$(".skills-box-2").hide();
+	$(".skills-box-3").hide();
+	$(".skills-box-4").hide();
+	$(".skills-box-5").hide();
+
+	$(".title-box").slideDown(800);
+	$(".skills-box-1").slideDown(850);
+	$(".skills-box-2").slideDown(900);
+	$(".skills-box-3").slideDown(950);
+	$(".skills-box-4").slideDown(1000);
+	$(".skills-box-5").slideDown(1050);
+});
