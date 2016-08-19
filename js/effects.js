8c81754114072bd7bb65b464aad2434849b367e3
JDjedi <JDjedi1201@gmail.com>
2016-08-18 23:04:15 -0500

    Amended JS file

--- js/effects.js
+++ js/effects.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
 	$(".skills-box-4").hide();
 	$(".skills-box-5").hide();
 
-	$(".title-box").slideDown(800);
-	$(".skills-box-1").slideDown(850);
-	$(".skills-box-2").slideDown(900);
-	$(".skills-box-3").slideDown(950);
-	$(".skills-box-4").slideDown(1000);
-	$(".skills-box-5").slideDown(1050);
+	$(".title-box").slideDown(1500);
+	$(".skills-box-1").slideDown(1525);
+	$(".skills-box-2").slideDown(1575);
+	$(".skills-box-3").slideDown(1650);
+	$(".skills-box-4").slideDown(1750);
+	$(".skills-box-5").slideDown(1875);
 });
